4ffe3fc9660c7653657c0294cc1a8dea1dd0a5e0
Jan Pajdak <pajdakjan@gmail.com>
2018-01-12 13:15:29 +0100

    Added ShowsTests

---
+++ CinemaTests/Containers/ShowsTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CinemaTests.Containers
+{
+    [TestClass()]
+    public class ShowsTests
+    {
+        [TestMethod()]
+        public void ShowsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PlannedShowsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
