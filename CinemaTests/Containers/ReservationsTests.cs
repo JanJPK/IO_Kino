33cde84afa5c44991d42088226e37688cf4e26e8
Jan Pajdak <pajdakjan@gmail.com>
2018-01-12 12:51:17 +0100

    Added unit test project.

---
+++ CinemaTests/Containers/ReservationsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CinemaTests.Containers
+{
+    [TestClass()]
+    public class ReservationsTests
+    {
+        [TestMethod()]
+        public void ReservationsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
