b36b0b91906c88344ba823c1949e438d7e93bf41
Jan Pajdak <pajdakjan@gmail.com>
2018-01-12 13:16:03 +0100

    Added rest of container test files.

---
+++ CinemaTests/Containers/MoviesTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CinemaTests.Containers
+{
+    [TestClass()]
+    public class MoviesTests
+    {
+        [TestMethod()]
+        public void MoviesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
